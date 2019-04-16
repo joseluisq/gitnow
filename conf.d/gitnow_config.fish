25df7bb9835be5003001cf34992d39155124a2db
Jose Quintana <joseluisquintana20@gmail.com>
2019-04-16 08:35:43 +0200

    2.0.6

--- conf.d/gitnow_config.fish
+++ conf.d/gitnow_config.fish
@@ -5,7 +5,7 @@ set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 set -q fish_config; or set -g fish_config $XDG_CONFIG_HOME/fish
 set -q fish_functions; or set -g fish_functions "$fish_config/functions"
 
-set -g gitnow_version 2.0.5
+set -g gitnow_version 2.0.6
 
 source "$fish_functions/__gitnow_functions.fish"
 source "$fish_functions/__gitnow_manual.fish"
