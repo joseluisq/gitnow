00f75c050d2102344fe078befc9c9dcf77497059
Jose Quintana <joseluisquintana20@gmail.com>
2020-08-18 17:33:09 +0200

    feat: include upstream and no-apply-stash completions for move cmd
    refer to #18

--- completions/__gitnow_completions.fish
+++ completions/__gitnow_completions.fish
@@ -1,3 +1,15 @@
 source $__fish_data_dir/completions/git.fish
 
-complete -f -c move -a '(__fish_git_branches)'
+complete -f -c move -a '( __fish_git_branches)'
+
+complete -f -c move \
+    -s u -l upstream \
+    -x \
+    -a '( __fish_git_branches)' \
+    -d "Fetch a remote branch and switch to it applying current stash"
+
+complete -f -c move \
+    -s n -l no-apply-stash \
+    -x \
+    -a '( __fish_git_branches)' \
+    -d "Switch to a local branch but without applying current stash" 
