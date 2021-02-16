4bc957b698a0403cf347704773c7cf8935fe21e8
Jose Quintana <joseluisquintana20@gmail.com>
2021-02-16 16:17:44 +0100

    test: fish files error checks

---
+++ testing.fish
@@ -0,0 +1,26 @@
+#!/usr/bin/fish
+
+# Archaic Fish shell error checks for GitNow,
+# since that Fish shell doesn't support any "strict mode" like Bash.
+# see https://github.com/fish-shell/fish-shell/issues/805
+
+echo "Testing GitNow files..."
+
+set -l out (fish -c 'source' 2>&1)
+
+if test $status -gt 0
+    echo "ERROR: Fish shell error signal was emitted!"
+    exit $status
+end
+
+# This is ad-hoc string check
+set -l err_str (echo $out | awk '$0 ~ /type: Unknown|fish: Unknown command:/ {print $1}')
+
+if test -n "$err_str"
+    echo "ERROR: Fish shell parsing error has occurred!"
+    exit 1
+end
+
+gitnow --version
+
+echo "OK"
