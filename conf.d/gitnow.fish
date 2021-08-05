0a87d53135ecc8066309c8c8f39c1796a7a4ab04
Lukas Gierth <lukas.gierth@posteo.de>
2021-08-05 17:45:44 +0200

    feat: '--tags' for auto push mode to include --follow-tags (pushes annotated tags) (#40)
    * 'push --tags' mode to include --follow-tags in push auto mode
    
    * cleanup: push auto/manual with switch
    
    * fix: reinclude echo and linebreaks
    
    * push: echo at the correct place
    
    * cleanup: remove commented part which is not used
    
    * fix: typo

--- conf.d/gitnow.fish
+++ conf.d/gitnow.fish
@@ -176,17 +176,41 @@ function push -d "Gitnow: Push commit changes to remote repository"
     set -l xorigin (__gitnow_current_remote)
     set -l xbranch (__gitnow_current_branch_name)
 
-    echo "🚀 Pushing changes..."
 
     if test (count $opts) -eq 0
         set opts $xorigin $xbranch
         set -l xremote_url (command git config --get "remote.$xorigin.url")
 
+        echo "🚀 Pushing changes..."
         echo "Mode: Auto"
         echo "Remote URL: $xorigin ($xremote_url)"
         echo "Remote branch: $xbranch"
     else
-        echo "Mode: Manual"
+        set -l v_mode "auto"
+
+        for v in $argv
+            switch $v
+                case -t --tags
+                    set opts $xorigin $xbranch --follow-tags
+                    set -l xremote_url (command git config --get "remote.$xorigin.url")
+
+                    echo "🚀 Pushing changes..."
+                    echo "Mode: Auto (incl. tags)"
+                    echo "Remote URL: $xorigin ($xremote_url)"
+                    echo "Remote branch: $xbranch"
+                case -h --help
+                    echo "NAME"
+                    echo "      Gitnow: push - Push current branch to default origin"
+                    echo "OPTIONS:"
+                    echo "      -t --tags               (auto mode) include annotated tags that relate to the commits"
+                    echo "      -h --help               Show information about the options for this command"
+                    return
+                case -\*
+                case '*'
+                    set -l v_mode "manual"
+                    echo "Mode: Manual"
+            end
+        end
     end
 
     echo
